0aa334ac0f77f218d1cc7fced5fcd3a09558a7ef
dhiastiii <dhiastiputri.jayanti@gmail.com>
2022-12-04 18:10:28 +0700

    Membuat POST dan PUT

---
+++ src/main/java/pws/c/restfull_projects/RestfullProjectsApplication.java
@@ -0,0 +1,13 @@
+package pws.c.restfull_projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws/c/restfull_projects/RestfullProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package pws.c.restfull_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
